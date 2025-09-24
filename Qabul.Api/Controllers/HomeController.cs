ecc2b39a9290f9723315f3b68c7f56ee5db2040a
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-09-24 15:46:07 +0500

    Qabul Api Project Initialize

---
+++ Qabul.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Qabul.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public ActionResult<string> Get()=>
+             "Hello Qabul";
+    }
+}
